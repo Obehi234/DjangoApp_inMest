02648d659023072dbc7ff553d9ff8533dbd9f2ea
obehi234 <obehi.ataga234@gmail.com>
2024-02-12 00:25:52 +0100

    set up class schedule model

--- main/models.py
+++ main/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class ClassSchedule(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
+    start_date_and_time = models.DateTimeField()
+    end_date_and_time = models.DateTimeField()
 
