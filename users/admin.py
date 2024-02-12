907b36a7a5e16b8f03860d6d5df08ff5be27c627
obehi234 <obehi.ataga234@gmail.com>
2024-02-12 02:09:03 +0100

    registered Cohort and CohortMember models

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from inmest.users.models import IMUser
+from inmest.users.models import Cohort, CohortMember, IMUser
 
 # Register your models here.
 
 admin.site.register(IMUser)
+admin.site.register(Cohort)
+admin.site.register(CohortMember)
