a6780c126950afdcceddcb1475a826595caea660
obehi234 <obehi.ataga234@gmail.com>
2024-02-12 01:55:30 +0100

    Added query user

--- main/models.py
+++ main/models.py
@@ -17,15 +17,27 @@ class ClassSchedule(models.Model):
 
 class ClassAttendance(models.Model):
     class_schedule = models.ForeignKey(ClassSchedule, on_delete=models.CASCADE)
-    description = models.TextField()
-    start_date_and_time = models.DateTimeField()
-    end_date_and_time = models.DateTimeField()
-    is_repeated = models.BooleanField(default=False)
-    repeat_frequency = models.CharField(max_length=50)
+    date_created = models.DateField()
+    date_modified = models.DateField() 
     is_present = models.BooleanField(default=True)
-    organizer= models.CharField(max_length=50)
-    attendee = models.ForeignKey(IMUser, on_delete=models.CASCADE),
-    venue = models.TextField()
+    attendee = models.ForeignKey(IMUser, on_delete=models.CASCADE), 
     author = models.ForeignKey(IMUser, on_delete=models.CASCADE, related_name = "cohort_member_author")
+
+class Query(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    submitted_by = models.ForeignKey(IMUser, on_delete=models.CASCADE, related_name = "query_submitted_by")
+    assigned_to = models.ForeignKey(IMUser, on_delete=models.CASCADE, related_name = "query_assigned_to")
+    resolution_status= [
+        ("PENDING", "PENDING"),
+        ("IN_PROGRESS", "IN_PROGRESS"),
+        ("DECLINED", "DECLINED"),
+        ("RESOLVED", "RESOLVED"),
+        
+    ]
+    author = models.ForeignKey(IMUser, on_delete=models.CASCADE, related_name = "query_author")
+
 
 
